bc6dc905cbd0ef9cf0f5633795c1cc9152cdb6de
mayur-dharukiya <mayur@revature.com>
2022-03-01 11:49:20 -0500

    second commit

---
+++ Sample1/src/revature/Tampa.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package revature;
+
+/**
+ * @author Mayura
+ *
+ */
+public class Tampa {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");;
+	}
+
+}
